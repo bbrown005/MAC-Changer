66c0c0289718f5de9873c991128ab4b4a4dbda0e
root <root@localhost.localdomain>
2018-09-11 01:28:25 -0400

    Initial Commit

---
+++ mac_changer.py
@@ -0,0 +1,12 @@
+#!/usr/bin.env python
+
+import subprocess
+
+interface = input("Interface > ")
+new_mac = input("New MAC > ")
+
+print("[+} Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
