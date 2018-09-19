1f9e79c5f20d320babea2fa97544f0da71c0a169
Bryan Brown <root@localhost.localdomain>
2018-09-19 02:11:02 -0400

    Third Commit, More Secure Versioning

--- mac_changer.py
+++ mac_changer.py
@@ -7,6 +7,12 @@ new_mac = input("New MAC > ")
 
 print("[+} Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+# Less secure version of MAC address changer
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+# More secure version
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
