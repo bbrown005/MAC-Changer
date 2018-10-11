e4f5fdd55254a5821c792ab6880b059b0a007702
Bryan Brown <root@localhost.localdomain>
2018-10-11 00:05:05 -0400

    Refactoring

--- mac_changer.py
+++ mac_changer.py
@@ -26,15 +26,17 @@ def change_mac(interface, new_mac):
 # End change_mac
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+# end get_current_mac
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 # change_mac(options.interface, options.new_mac)
-
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address")
