a52c37f68e4870485589636515fe8898dab2b0b2
Bryan Brown <bbrown004@student.franciscan.edu>
2018-12-12 01:25:52 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -19,7 +19,7 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac):
-    print("[+} Changing MAC address for " + interface + " to " + new_mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
